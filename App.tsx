63bc7d08c1b392952e0e4a02d7c7004fe762a39b
KELVIN GUIMARÃES MELO <kelvingui18@gmail.com>
2025-03-30 12:24:23 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Image, TextInput, Pressable, Button } from "react-native";
+import { styles } from ".";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/Group 329.png")}
+        style={{ margin: 20 }}
+      />
+
+      <View style={{ alignItems: "center", marginBottom: 20 }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+          Bem-vindo(a) de volta!
+        </Text>
+        <Text style={{ fontSize: 16, color: "gray" }}>
+          Acesse sua conta para continuar
+        </Text>
+      </View>
+
+      <TextInput
+        style={styles.email}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoComplete="email"
+      />
+      <TextInput
+        style={styles.senha}
+        placeholder="Senha"
+        autoCapitalize="none"
+        secureTextEntry
+      />
+
+      <Pressable
+        style={{
+          width: "70%",
+          borderRadius: 10,
+          marginBottom: 20,
+          marginTop: 10,
+        }}
+      >
+        <View style={{ borderRadius: 10, overflow: "hidden" }}>
+          <Button color={"#454B60"} title="Entrar" onPress={() => {}} />
+        </View>
+      </Pressable>
+
+      <Text style={{ fontSize: 16, color: "gray" }}>continuar com:</Text>
+
+      <View style={styles.options}>
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 10,
+            backgroundColor: "#454B60",
+            borderRadius: 10,
+          }}
+          onPress={() => {}}
+        >
+          <Image
+            source={require("./assets/facebook.png")}
+            style={{ width: 20, height: 20, marginRight: 10 }}
+          />
+          <Text style={{ color: "white" }}>Facebook</Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 10,
+            backgroundColor: "#ffffff",
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: "gray",
+          }}
+          onPress={() => {}}
+        >
+          <Image
+            source={require("./assets/google.png")}
+            style={{ width: 20, height: 20, marginRight: 10 }}
+          />
+          <Text style={{ color: "#454B60" }}>Google</Text>
+        </Pressable>
+      </View>
+
+      <Pressable style={styles.subConteiner} onPress={() => {}}>
+        <Text>Esqueceu a senha?</Text>
+        <Text>Criar uma conta</Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- index.ts
+++ index.ts
@@ -1,8 +1,61 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import { StyleSheet } from 'react-native';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  email: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    width: '80%',
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+  },
+  senha: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    width: '80%',
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#ffffff'
+  },
+
+  login: {
+    width: '80%',
+    borderRadius: 1,
+    backgroundColor: '#454B60',
+    marginBottom: 10,
+  },
+
+  subConteiner: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginTop: 10,
+  },
+  
+  options: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
